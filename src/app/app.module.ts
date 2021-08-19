d5594f585e3cdefb6db764fa6f6f98f19c29e3b1
arostapov <arostapov@gmail.com>
2021-08-19 20:31:15 +0300

    update styles change tsconfig remove useless imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from "./modules/home/home.module";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {HttpClientModule} from "@angular/common/http";
-import {CoreModule} from "./core/core.module";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     CoreModule,
